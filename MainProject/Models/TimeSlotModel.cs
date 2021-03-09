a0b4548cdd2f8f9c3242502ed17a91c73cd313cf
CasLeeflang <466933@student.fontys.nl>
2021-03-09 17:00:54 +0100

    Timeslot creation

--- MainProject/Models/TimeSlotModel.cs
+++ MainProject/Models/TimeSlotModel.cs
@@ -9,7 +9,6 @@ namespace Models
         public string DayOTWeek { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
-
         public int NumberOfSpotsAvailable { get; set; }
 
     }
